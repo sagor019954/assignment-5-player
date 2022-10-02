be687b4b9cf0fe424cb427c078b6d913e38fe717
sohel0199 <sohel019954@gmail.com>
2022-10-02 15:31:33 +0600

    all done

--- club.js
+++ club.js
@@ -1,19 +1,24 @@
 function TotalCosts() {
+    // get per person value
     const perPerson = document.getElementById('per-person');
     const perPersonString = perPerson.value;
     const perPersonCost = parseFloat(perPersonString);
     const playerPriceString = playerNewArray.length;
     const playerPrice = parseFloat(playerPriceString);
     const totalPrice = perPersonCost * playerPrice;
+    // set the total expanses value 
     const totalExpanses = document.getElementById('total-expanses');
     totalExpanses.innerText = totalPrice
+    // get input manager value 
     const inputMan = document.getElementById('input-Man');
     const inputManString = inputMan.value;
     const inputManValue = parseFloat(inputManString);
+    // get input coach value 
     const inputCoach = document.getElementById('input-coach');
     const inputCoachString = inputCoach.value;
     const inputCoachValue = parseFloat(inputCoachString)
     const sum = inputManValue + inputCoachValue + totalPrice;
+    // set all value in total
     const totalfield = document.getElementById('total-field');
     totalfield.innerText = sum;
     if (isNaN(perPersonCost)) {
@@ -24,7 +29,7 @@ function TotalCosts() {
         alert('Give Maneger and Coach Price')
     }
 }
-
+// button Clicked complete
 const playerNewArray = [];
 function addName(player) {
     playerNewArray.push(player);
@@ -38,16 +43,13 @@ function addName(player) {
     li.innerText = player;
     ul.appendChild(li);
     playerList.appendChild(ul);
-    console.log(playerNewArray);
 }
+// The text by class
 const btns = document.getElementsByClassName('btn');
-
 const names = document.getElementsByClassName('name');
 for (let i = 0; i <= btns.length; i++) {
-
     btns[i].addEventListener('click', function () {
         const playerName = names[i];
-        console.log(playerName);
         const player = playerName.innerText;
         addName(player)
         if (true && playerNewArray.length <= 5) {
